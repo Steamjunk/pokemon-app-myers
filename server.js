3822e1fd7ae118a6ded5b4a9bbdb6cc6d7451743
Chris Myers <myerschristopher@johndeere.com>
2020-10-02 09:46:42 -0500

    dotenv fix

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+require('dotenv').config();
 const methodOverride = require('method-override');
 const routes = require('./routes');
 const app = express();
@@ -11,6 +12,6 @@ app.use('/pokemon', routes.pokemon);
 app.use('/players', routes.players);
 app.use('/teams', routes.teams);
 
-app.listen(3000, () => {
-    console.log('My server is up and running');
+app.listen(process.env.PORT, () => {
+    console.log(`My server is up and running on port ${process.env.PORT}`);
 })
